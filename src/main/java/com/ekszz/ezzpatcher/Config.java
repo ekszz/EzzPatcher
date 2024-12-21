683425d7885c4b1cda28680adb4f9995402499df
Ekszz <ekszz@users.noreply.github.com>
2024-12-21 14:56:13 +0800

    feat: Supports overwriting the entire class using the class's bytecode

--- src/main/java/com/ekszz/ezzpatcher/Config.java
+++ src/main/java/com/ekszz/ezzpatcher/Config.java
@@ -103,78 +103,92 @@ public class Config implements Serializable {
                 Map<String, ?> yamlClassPatchDefine = (Map<String, ?>) yamlData.get("classPatchDefine");
                 for (String perClass : yamlClassPatchDefine.keySet()) {
                     // process every class
-                    if (!(yamlClassPatchDefine.get(perClass) instanceof List)) {
-                        Log.warn("[-] Invalid config at classPatchDefine, it must be a LIST, skip.");
-                        continue;
-                    }
-                    List yamlClassObjList = (List) yamlClassPatchDefine.get(perClass);
-                    List<ClassMethodDefine> newPerClassMethodDefine = new ArrayList<>();
-                    for (int i = 0; i < yamlClassObjList.size(); i++) {
-                        // process every method
-                        if (!(yamlClassObjList.get(i) instanceof Map)) {
-                            Log.warn("[-] Invalid class define on {} at index {}, skip.", perClass, i);
-                            continue;
-                        }
-                        Map<String, ?> yamlClassObj = (Map<String, ?>) yamlClassObjList.get(i);
-                        if (!yamlClassObj.containsKey("method") || !yamlClassObj.containsKey("code")
-                                || !(yamlClassObj.get("method") instanceof String)
-                                || !(yamlClassObj.get("code") instanceof String)) {
-                            Log.warn("[-] Invalid required parameter METHOD or CODE on {} at index {}, skip.",
-                                    perClass, i);
-                            continue;
-                        }
-                        if (yamlClassObj.containsKey("mode") &&
-                                (!(yamlClassObj.get("mode") instanceof String) ||
-                                        (!"overwrite".equals(yamlClassObj.get("mode"))
-                                                && !"insertAt".equals(yamlClassObj.get("mode"))
-                                                && !"insertBefore".equals(yamlClassObj.get("mode"))
-                                                && !"insertAfter".equals(yamlClassObj.get("mode"))))) {
-                            Log.warn("[-] parameter MODE on {} at index {} must be overwrite, insertAt," +
-                                    " insertBefore or insertAfter, skip.", perClass, i);
-                            continue;
-                        }
-                        Integer insertAt = null;
-                        if ("insertAt".equals(yamlClassObj.get("mode"))) {
-                            if (!yamlClassObj.containsKey("insertAt")) {
-                                Log.warn("[-] when MODE is insertAt, parameter INSERTAT is needed," +
-                                        " on {} at index {}.", perClass, i);
+                    if (yamlClassPatchDefine.get(perClass) instanceof List) {
+                        List yamlClassObjList = (List) yamlClassPatchDefine.get(perClass);
+                        List<ClassMethodDefine> newPerClassMethodDefine = new ArrayList<>();
+                        for (int i = 0; i < yamlClassObjList.size(); i++) {
+                            // process every method
+                            if (!(yamlClassObjList.get(i) instanceof Map)) {
+                                Log.warn("[-] Invalid class define on {} at index {}, skip.", perClass, i);
                                 continue;
                             }
-                            if (yamlClassObj.get("insertAt") instanceof Integer) {
-                                insertAt = (Integer) yamlClassObj.get("insertAt");
+                            Map<String, ?> yamlClassObj = (Map<String, ?>) yamlClassObjList.get(i);
+                            if (!yamlClassObj.containsKey("method") || !yamlClassObj.containsKey("code")
+                                    || !(yamlClassObj.get("method") instanceof String)
+                                    || !(yamlClassObj.get("code") instanceof String)) {
+                                Log.warn("[-] Invalid required parameter METHOD or CODE on {} at index {}, skip.",
+                                        perClass, i);
+                                continue;
+                            }
+                            if (yamlClassObj.containsKey("mode") &&
+                                    (!(yamlClassObj.get("mode") instanceof String) ||
+                                            (!"overwrite".equals(yamlClassObj.get("mode"))
+                                                    && !"insertAt".equals(yamlClassObj.get("mode"))
+                                                    && !"insertBefore".equals(yamlClassObj.get("mode"))
+                                                    && !"insertAfter".equals(yamlClassObj.get("mode"))))) {
+                                Log.warn("[-] parameter MODE on {} at index {} must be overwrite, insertAt," +
+                                        " insertBefore or insertAfter, skip.", perClass, i);
+                                continue;
+                            }
+                            Integer insertAt = null;
+                            if ("insertAt".equals(yamlClassObj.get("mode"))) {
+                                if (!yamlClassObj.containsKey("insertAt")) {
+                                    Log.warn("[-] when MODE is insertAt, parameter INSERTAT is needed," +
+                                            " on {} at index {}.", perClass, i);
+                                    continue;
+                                }
+                                if (yamlClassObj.get("insertAt") instanceof Integer) {
+                                    insertAt = (Integer) yamlClassObj.get("insertAt");
+                                } else {
+                                    Log.warn("[-] parameter INSERTAT must be a number, on {} at index {}.", perClass, i);
+                                    continue;
+                                }
+                            }
+                            if (yamlClassObj.containsKey("paramType")
+                                    && !(yamlClassObj.get("paramType") instanceof List)) {
+                                Log.warn("[-] parameter PARAMTYPE must be a LIST on {} at index {}, skip.",
+                                        perClass, i);
+                                continue;
+                            }
+                            ClassMethodDefine x = new ClassMethodDefine();
+                            x.setMethod((String) yamlClassObj.get("method"));
+                            x.setCode((String) yamlClassObj.get("code"));
+                            if (yamlClassObj.containsKey("mode")) {
+                                x.setMode((String) yamlClassObj.get("mode"));
+                                if ("insertAt".equals(x.getMode())) {
+                                    x.setInsertAt(insertAt);
+                                }
                             } else {
-                                Log.warn("[-] parameter INSERTAT must be a number, on {} at index {}.", perClass, i);
-                                continue;
+                                x.setMode("overwrite");
                             }
-                        }
-                        if (yamlClassObj.containsKey("paramType")
-                                && !(yamlClassObj.get("paramType") instanceof List)) {
-                            Log.warn("[-] parameter PARAMTYPE must be a LIST on {} at index {}, skip.",
-                                    perClass, i);
-                            continue;
-                        }
-                        ClassMethodDefine x = new ClassMethodDefine();
-                        x.setMethod((String) yamlClassObj.get("method"));
-                        x.setCode((String) yamlClassObj.get("code"));
-                        if (yamlClassObj.containsKey("mode")) {
-                            x.setMode((String) yamlClassObj.get("mode"));
-                            if ("insertAt".equals(x.getMode())) {
-                                x.setInsertAt(insertAt);
+                            if (yamlClassObj.containsKey("paramType")) {
+                                List<String> paramType = new ArrayList<>();
+                                List t = (List) yamlClassObj.get("paramType");
+                                for (Object o : t) {
+                                    paramType.add(o.toString());
+                                }
+                                x.setParamType(paramType);
                             }
-                        } else {
-                            x.setMode("overwrite");
+                            newPerClassMethodDefine.add(x);
                         }
-                        if (yamlClassObj.containsKey("paramType")) {
-                            List<String> paramType = new ArrayList<>();
-                            List t = (List) yamlClassObj.get("paramType");
-                            for (Object o : t) {
-                                paramType.add(o.toString());
-                            }
-                            x.setParamType(paramType);
+                        newClassPatchDefine.put(perClass.replace(".", "/"), newPerClassMethodDefine);
+                    } else if (yamlClassPatchDefine.get(perClass) instanceof String) {
+                        // base64
+                        String yamlClassObjString = (String) yamlClassPatchDefine.get(perClass);
+                        try {
+                            Base64.getDecoder().decode(yamlClassObjString);
+                            List<ClassMethodDefine> newPerClassMethodDefine = new ArrayList<>();
+                            ClassMethodDefine x = new ClassMethodDefine();
+                            x.setMethod("_");
+                            x.setCode(yamlClassObjString);
+                            newPerClassMethodDefine.add(x);
+                            newClassPatchDefine.put(perClass.replace(".", "/"), newPerClassMethodDefine);
+                        } catch (IllegalArgumentException e) {
+                            Log.warn("[-] Invalid base64 of class bytes define on {}, skip.", perClass);
                         }
-                        newPerClassMethodDefine.add(x);
+                    } else {
+                        Log.warn("[-] Invalid config at classPatchDefine, it must be a LIST or BASE64, skip.");
                     }
-                    newClassPatchDefine.put(perClass.replace(".", "/"), newPerClassMethodDefine);
                 }
             }
         }
--- src/main/java/com/ekszz/ezzpatcher/EzzTransformer.java
+++ src/main/java/com/ekszz/ezzpatcher/EzzTransformer.java
@@ -6,6 +6,7 @@ import java.lang.instrument.ClassFileTransformer;
 import java.security.ProtectionDomain;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.List;
 
 public class EzzTransformer implements ClassFileTransformer {
@@ -37,41 +38,49 @@ public class EzzTransformer implements ClassFileTransformer {
                     config.getBackupCode().put(className, Arrays.copyOf(classfileBuffer, classfileBuffer.length));
                 }
                 try {
-                    ClassPool classPool = new ClassPool(ClassPool.getDefault());
-                    classPool.insertClassPath(new ByteArrayClassPath(dotClassName, config.getBackupCode().get(className)));
-                    ClassClassPath classPath = new ClassClassPath(this.getClass());
-                    classPool.appendClassPath(classPath);
-                    classPool.appendClassPath(new LoaderClassPath(loader));
+                    if (classMethodDefineList.size() == 1 && "_".equals(classMethodDefineList.get(0).getMethod())) {
+                        // overwrite class
+                        byte[] bytes = Base64.getDecoder().decode(classMethodDefineList.get(0).getCode());
+                        Log.info("[+] Hook Ready: {}, {} bytes.", dotClassName, bytes.length);
+                        return bytes;
+                    } else {
+                        // modify every method
+                        ClassPool classPool = new ClassPool(ClassPool.getDefault());
+                        classPool.insertClassPath(new ByteArrayClassPath(dotClassName, config.getBackupCode().get(className)));
+                        ClassClassPath classPath = new ClassClassPath(this.getClass());
+                        classPool.appendClassPath(classPath);
+                        classPool.appendClassPath(new LoaderClassPath(loader));
 
-                    CtClass clazz = classPool.get(dotClassName);
-                    List<String> res = new ArrayList<>();
-                    for (ClassMethodDefine classMethodDefine : classMethodDefineList) {
-                        CtMethod targetMethod;
-                        if (classMethodDefine.getParamType() == null) {
-                            targetMethod = clazz.getDeclaredMethod(classMethodDefine.getMethod());
-                        } else {
-                            CtClass[] paramType = new CtClass[classMethodDefine.getParamType().size()];
-                            for (int i = 0; i < classMethodDefine.getParamType().size(); i++) {
-                                paramType[i] = classPool.get(classMethodDefine.getParamType().get(i));
+                        CtClass clazz = classPool.get(dotClassName);
+                        List<String> res = new ArrayList<>();
+                        for (ClassMethodDefine classMethodDefine : classMethodDefineList) {
+                            CtMethod targetMethod;
+                            if (classMethodDefine.getParamType() == null) {
+                                targetMethod = clazz.getDeclaredMethod(classMethodDefine.getMethod());
+                            } else {
+                                CtClass[] paramType = new CtClass[classMethodDefine.getParamType().size()];
+                                for (int i = 0; i < classMethodDefine.getParamType().size(); i++) {
+                                    paramType[i] = classPool.get(classMethodDefine.getParamType().get(i));
+                                }
+                                targetMethod = clazz.getDeclaredMethod(classMethodDefine.getMethod(), paramType);
                             }
-                            targetMethod = clazz.getDeclaredMethod(classMethodDefine.getMethod(), paramType);
+                            // modify method code
+                            if ("insertBefore".equals(classMethodDefine.getMode())) {
+                                targetMethod.insertBefore(classMethodDefine.getCode());
+                            } else if ("insertAfter".equals(classMethodDefine.getMode())) {
+                                targetMethod.insertAfter(classMethodDefine.getCode());
+                            } else if ("insertAt".equals(classMethodDefine.getMode())) {
+                                targetMethod.insertAt(classMethodDefine.getInsertAt(), classMethodDefine.getCode());
+                            } else {
+                                targetMethod.setBody(classMethodDefine.getCode());
+                            }
+                            res.add(classMethodDefine.getMethod());
                         }
-                        // modify method code
-                        if ("insertBefore".equals(classMethodDefine.getMode())) {
-                            targetMethod.insertBefore(classMethodDefine.getCode());
-                        } else if ("insertAfter".equals(classMethodDefine.getMode())) {
-                            targetMethod.insertAfter(classMethodDefine.getCode());
-                        } else if ("insertAt".equals(classMethodDefine.getMode())) {
-                            targetMethod.insertAt(classMethodDefine.getInsertAt(), classMethodDefine.getCode());
-                        } else {
-                            targetMethod.setBody(classMethodDefine.getCode());
-                        }
-                        res.add(classMethodDefine.getMethod());
+                        byte[] byteCode = clazz.toBytecode();
+                        clazz.detach();
+                        Log.info("[+] Hook ready: {} on methods {}.", dotClassName, res);
+                        return byteCode;
                     }
-                    byte[] byteCode = clazz.toBytecode();
-                    clazz.detach();
-                    Log.info("[+] Hook success: {} on methods {}.", dotClassName, res);
-                    return byteCode;
                 } catch (Throwable e) {
                     Log.warn("[-] Hook fail: {}", dotClassName);
                     Log.warn(e);
